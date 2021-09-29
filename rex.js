3b31d1166e95edc424e66fc696483fe9d2500f39
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-09-29 11:20:14 +0530

    🍁🍁

--- rex.js
+++ rex.js
@@ -1,13 +1,13 @@
-/* Copyright (C) 2021 TENUX-Neotro.
+/* Copyright (C) 2021 HIRUWA - TREX
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-NEOTROX - TEENUHX
+HIRUWA -T REX
 */
 
 var config = require('./config');
 var Commands = [];
 
-function teenu(info, func) {
+function Trex(info, func) {
     // Basit bir fonksiyon, komut eklemek için.
     var types = ['photo', 'image', 'text', 'message'];
 
@@ -42,6 +42,6 @@ function teenu(info, func) {
 }
 
 module.exports = {
-    teenu: teenu,
+    teenu: Trex,
     commands: Commands
 }
