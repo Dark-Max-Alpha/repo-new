8761458eec8eb9d123b6fc4dc57c66a073b91449
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-10-01 09:01:52 +0530

    😈😈

--- plugins/Easyname.js
+++ plugins/Easyname.js
@@ -10,21 +10,21 @@ const axios = require('axios');
 
 Trex.addrex({pattern: 'public', fromMe: true,  deleteCommand: true,  desc: 'short cut'}, (async (message, match) => {    
     
-        await message.client.sendMessage(message.jid,'.setvar WORK_TYPE:public', MessageType.text);
+        await message.client.sendMessage(message.jid,'.setvar WORK_TYPE=public', MessageType.text);
         await message.client.sendMessage(message.jid,'YOUR BOT SET ON PUBLIC 👿👿',MessageType.text);
             
         }));
 
 Trex.addrex({pattern: 'private', fromMe: true,  deleteCommand: true,  desc: 'short cut'}, (async (message, match) => {    
     
-        await message.client.sendMessage(message.jid,'.setvar WORK_TYPE:private', MessageType.text);
+        await message.client.sendMessage(message.jid,'.setvar WORK_TYPE=private', MessageType.text);
         await message.client.sendMessage(message.jid,'YOUR BOT SET ON PRIVATE 👿👿',MessageType.text);
             
         }));
 
 Trex.addrex({pattern: 'voice on', fromMe: true,  deleteCommand: true,  desc: 'short cut'}, (async (message, match) => {    
     
-        await message.client.sendMessage(message.jid,'.setvar VOICE_CHAT:true', MessageType.text);
+        await message.client.sendMessage(message.jid,'.setvar VOICE_CHAT=true', MessageType.text);
         await message.client.sendMessage(message.jid,'ACTIVATED VOICE CHAT 👿👿',MessageType.text);
             
         }));
@@ -32,7 +32,7 @@ Trex.addrex({pattern: 'voice on', fromMe: true,  deleteCommand: true,  desc: 'sh
 
 Trex.addrex({pattern: 'voice off', fromMe: true,  deleteCommand: true,  desc: 'short cut'}, (async (message, match) => {    
     
-        await message.client.sendMessage(message.jid,'.setvar VOICE_CHAT:false', MessageType.text);
+        await message.client.sendMessage(message.jid,'.setvar VOICE_CHAT=false', MessageType.text);
         await message.client.sendMessage(message.jid,'DEACTIVATED VOICE CHAT 👿👿',MessageType.text);
             
         }));
