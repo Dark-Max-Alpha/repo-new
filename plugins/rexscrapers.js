aea09b5225f65e001b7f1e7b843f1dd6ed3eebd2
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-10-01 13:13:01 +0530

    👿👿

--- plugins/rexscrapers.js
+++ plugins/rexscrapers.js
@@ -363,7 +363,7 @@ Trex.addrex({pattern: 'play ?(.*)', fromMe: true, desc: Lang.PLAY_DESC}, (async 
         await message.client.sendMessage(message.jid, info, MessageType.text);
         await reply.delete();
     }));
-
+    
     Trex.addrex({pattern: 'img ?(.*)', fromMe: true, desc: Lang.IMG_DESC}, (async (message, match) => { 
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);
@@ -380,6 +380,7 @@ Trex.addrex({pattern: 'play ?(.*)', fromMe: true, desc: Lang.PLAY_DESC}, (async 
             message.reply(Lang.IMG.format((result.length < 5 ? result.length : 5), match[1]));
         });
     }));
+    
     Trex.addrex({pattern: 'quote ?(.*)', fromMe: true,  deleteCommand: false, desc: Lang.QUOTE_DESC}, async (message, match) => {
         if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
         const url = `https://api.quotable.io/random`;
@@ -841,14 +842,14 @@ else if (config.WORKTYPE == 'public') {
 
     Trex.addrex({pattern: 'img ?(.*)', fromMe: false, desc: Lang.IMG_DESC}, (async (message, match) => { 
 
-        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text, {quoted: message.data});
+        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORDS,MessageType.text);
         gis(match[1], async (error, result) => {
             for (var i = 0; i < (result.length < 5 ? result.length : 5); i++) {
                 var get = got(result[i].url, {https: {rejectUnauthorized: false}});
                 var stream = get.buffer();
                 
                 stream.then(async (image) => {
-                    await message.client.sendMessage(message.jid,image, MessageType.image, {quoted: message.data });
+                    await message.client.sendMessage(message.jid,image, MessageType.image, {quoted: message.data});
                 });
             }
 
