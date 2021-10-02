d2ed65fc528d337c33e16aec39e8a66ba5baa6bb
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-10-02 18:07:42 +0530

    Add files via upload

---
+++ plugins/status.js
@@ -0,0 +1,37 @@
+const Trex = require('../events');
+const {MessageType, Mimetype} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
+const {execFile} = require('child_process');
+const cwebp = require('cwebp-bin');
+const Config = require('../config');
+const Down = '👿 DOWNLOADING STATUS 👿.'
+
+    Trex.addrex({pattern: 'down$', fromMe: true }, (async (message, match) => {    
+
+        if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
+        var downloading = await message.client.sendMessage(message.jid,Down,MessageType.text);
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
+
+        if (message.reply_message.video === false && message.reply_message.image) {
+            ffmpeg(location)
+                .save('status.png')
+                .on('end', async () => {
+                    await message.client.sendMessage(message.client.user.jid,fs.readFileSync('status.png'), MessageType.image,{mimetype: Mimetype.png, caption: '❰🍁🔱  T Rex BOT  🔱🍁❱' ,quoted: message.data});
+            });
+        return 
+        }
+
+        ffmpeg(location)
+            .save('statu.mp4')
+            .on('end', async () => {
+                await message.client.sendMessage(message.client.user.jid,fs.readFileSync('statu.mp4'), MessageType.video,{mimetype: Mimetype.mp4, caption: '❰🍁🔱  T Rex BOT  🔱🍁❱' ,quoted: message.data});
+            });
+        return 
+    }));
