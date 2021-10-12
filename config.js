f55da0b35fb4c17750bd2ef73c200725e0de9f89
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-10-12 16:32:33 +0530

    Added ALL CONTRY TIME ZONES

--- config.js
+++ config.js
@@ -20,6 +20,7 @@ module.exports = {
     CHANNEL: 'https://chat.whatsapp.com/GT5V8RakkftB7DAKWMeQML',
     SESSION: process.env.TREX_SESSION === undefined ? '' : process.env.TREX_SESSION,
     ANTİLİNK: process.env.ANTİ_LİNK === undefined ? 'false' : process.env.ANTİ_LİNK,
+    TIME: process.env.TIME_ZONE === undefined ? 'Asia/Colombo' : process.env.TIME_ZONE,
     BTN: process.env.BUTTON_CMD === undefined ? 'btn' : process.env.BUTTON_CMD,
     AUTOBIO: process.env.AUTO_BIO === undefined ? 'false' : process.env.AUTO_BIO,
     THERI_KICK: process.env.THERI_KICK === undefined ? 'false' : process.env.THERI_KICK,
