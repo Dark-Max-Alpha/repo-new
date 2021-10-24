2593387be08764b997f820d6ac3bd95bcf2f04c1
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-10-24 08:43:09 +0530

    You can use tag cmd without admin now

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -14,8 +14,8 @@ async function checkImAdmin(message, user = message.client.user.jid) {
 }
 
 Trex.addrex({pattern: 'tagall ?(.*)', fromMe: true,  deleteCommand: false,  desc: Lang.TAGALL_DESC, dontAddCommandList: true}, (async (message, match) => {
-    var im = await checkImAdmin(message);
-    if (!im) return await message.client.sendMessage(message.jid,Lang.ADMİN,MessageType.text);
+//    var im = await checkImAdmin(message);
+//    if (!im) return await message.client.sendMessage(message.jid,Lang.ADMİN,MessageType.text);
 
     if (!message.reply_message) {
         if (match[1] !== '') {
