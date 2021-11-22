57ea1042b356ab2b5e6f342e726258b1f220d68d
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-11-22 16:57:37 +0530

    Don't update

---
+++ plugins/Vid.js
@@ -0,0 +1,31 @@
+const Trex = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const { errorMessage, infoMessage } = require('../helpers');
+const Config = require('../config');
+const Language = require('../language');
+const Lang = Language.getString('Trex');
+const YTV_DESC = "❤❤"
+const YT_NEED = "*Need song Name or Link!.*"
+const NO_RESULT = "*🌀can't Find Anything...*"
+const tk = Config.WORKTYPE == 'public' ? false : true
+
+    Trex.addrex({ pattern: 'video ?(.*)', fromMe: true, deleteCommand: false, desc: Lang.MP4,  deleteCommand: false}, async (message, match) => {
+        const linkk = match[1]
+        if (!linkk) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text)
+        await message.client.sendMessage(message.jid,Config.VD,MessageType.text);
+        await axios
+          .get(`https://rei-api.herokuapp.com/api/dl/ytavv2?url=${linkk}`)
+          .then(async (response) => {
+            const {
+              link,
+            } = response.data.result
+            const videoBuffer = await axios.get(link, {responseType: 'arraybuffer'})
+            await message.client.sendMessage(message.jid,Config.VU,MessageType.text);
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.video, {mimetype: Mimetype.mp4, ptt: false,quoted: message.data  })
+        })
+        .catch(
+          async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
+        )
+      },
+    )
