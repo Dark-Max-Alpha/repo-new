5600a3f0dfd07ce3ed9aadeb0e234a19144ee35a
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-12-04 15:08:27 +0530

    🎅❄️☃️ MERRY CHRISTMAS ❄️☃️

--- events.js
+++ events.js
@@ -1,7 +1,7 @@
 /* Copyright (C) 2020 T-REX HIRUWA
 
 Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
+you may not use this file except in compliance with the License
 
 T-REX HIRUWA
 */
