ad851c6964ecfa72f12a94f12f391fe45d0f6f1e
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-12-04 15:10:00 +0530

    🎅❄️☃️ MERRY CHRISTMAS ❄️☃️

--- hiruwa.js
+++ hiruwa.js
@@ -1,7 +1,6 @@
-/* Copyright (C) 2021 TENUX-Neotro.
+/* Copyright (C) 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-NEOTROX - TEENUHX
 */
 
 // Komutları burada tutacağız.
