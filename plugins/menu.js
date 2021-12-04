5d1085e732c55cf6707d2c397c951e3bb3afc6e3
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2021-12-04 14:29:50 +0530

    │  │🎅❄️☃️ MERRY CHRISTMAS ❄️☃️  │

--- plugins/menu.js
+++ plugins/menu.js
@@ -39,12 +39,15 @@ if (hrs >= 17 && hrs <= 19) wish = '*ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 🌥*'
 if (hrs >= 19 && hrs <= 24) wish = '*ɢᴏᴏᴅ ɴɪɢʜᴛ 🌙*'
     
     const rows = [
-        {title: '👿 ALL MENU 👿', description: `▷▷▷▷▷▷\n\n\n
+        {title: '🎅 ALL MENU ❄️☃️', description: `▷▷▷▷▷▷\n\n\n
  ╭────────────────
  │❰🍁🔱  T Rex BOT  🔱🍁❱
  ╰────────────────
  ╭────────────────
  │ Hello ▷ Dear
+ │
+ │🎅❄️☃️ MERRY CHRISTMAS ❄️☃️
+ │
  │ I Wish `+ wish + `
  │ 
  │Time⌚` + time + `
@@ -248,7 +251,7 @@ if (hrs >= 19 && hrs <= 24) wish = '*ɢᴏᴏᴅ ɴɪɢʜᴛ 🌙*'
   
        👿       Hiruwa        👿
 ` , rowId:"rowid1"},
-        {title: '👿 ADVANCE MENU 👿', description:`▷▷▷▷▷▷\n\n\n
+        {title: '🎅 ADVANCE MENU ❄️☃️', description:`▷▷▷▷▷▷\n\n\n
 ╔═══════════════════════╗
 
            ❰🍁🔱  T Rex BOT  🔱🍁❱
@@ -543,7 +546,7 @@ Example : .covid Sri Lanka°
 
 *➢ Description:* °Shorten link.°
 `, rowId:"rowid3"},
-        {title: '👿 LOGO PACK 01 👿', description: `▷▷▷▷▷▷\n\n\n
+        {title: '🎅 LOGO PACK 01 ❄️☃️', description: `▷▷▷▷▷▷\n\n\n
 ╔══════════════════════
 ║❌ 🍁*REX LOGO PACK 1*🍁 ❌
 ╠══════════════════════
@@ -634,7 +637,7 @@ Example : .covid Sri Lanka°
 ║        ╚ ENJOY WITH US ╝
 ╚══════════════════════
 `, rowId:"rowid4"},
-        {title: '👿 FF PACK 👿', description:`▷▷▷▷▷▷\n\n\n
+        {title: '☃️ FF PACK ❄️🎅', description:`▷▷▷▷▷▷\n\n\n
 ╔═════════════════════╗
 ║      ❰🍁🔱  T Rex BOT  🔱🍁❱  
 ║         ║🍁╚FF PACK╝🍁║
@@ -744,7 +747,7 @@ Example : .covid Sri Lanka°
 ║ ❰🍁🔱  T Rex BOT  🔱🍁❱  
 ╚═════════════════════╝
 `, rowId:"rowid5"},
-        {title: '👿 LOGO PACK 02 👿', description: `▷▷▷▷▷▷\n\n\n
+        {title: '☃️ LOGO PACK 02 ❄️🎅', description: `▷▷▷▷▷▷\n\n\n
 ╔══════════════════════
 ║❌ 🍁*REX LOGO PACK 2*🍁 ❌
 ╠══════════════════════
@@ -839,7 +842,7 @@ Example : .covid Sri Lanka°
 ║        ╚ ENJOY WITH US ╝
 ╚══════════════════════
 `, rowId:"rowid6"},
-        {title: '👿 ATTP PACK 👿', description: `▷▷▷▷▷▷\n\n\n
+        {title: '🎅 ATTP PACK ❄☃️', description: `▷▷▷▷▷▷\n\n\n
 ╔═════════════════════╗
 ║      ❰🍁🔱  T Rex BOT  🔱🍁❱  
 ║        ║🍁╚ATTP PACK╝🍁║
@@ -931,7 +934,7 @@ Example : .covid Sri Lanka°
 ╚═════════════════════╝
 
 `, rowId:"rowid20"},
-        {title: '👿 DOWNLOADER 👿', description: `▷▷▷▷▷▷\n\n\n
+        {title: '☃️ DOWNLOADER 🎅❄️', description: `▷▷▷▷▷▷\n\n\n
  ╭────────────────
  │  🍁👿 Downloader 👿🍁
  │
@@ -949,7 +952,7 @@ Example : .covid Sri Lanka°
  │
  ╰────────────────
 `, rowId:"rowid9"},
-        {title: '👿 ABOUT T-REX BOT 👿', description:`▷▷▷▷▷▷\n\n\n
+        {title: '☃️ ABOUT T-REX BOT 🎅❄️', description:`▷▷▷▷▷▷\n\n\n
 🍁 Developer : Hiruwa
 🍁 Bot             : T-REX V 3.0.0 
 🍁 Language : Sinhala & English
@@ -959,11 +962,11 @@ Example : .covid Sri Lanka°
 `, rowId:"rowid2"}
        ]
        
-       const sections = [{title: "👿 T-REX Command Panel 👿", rows: rows}]
+       const sections = [{title: "☃️ T-REX Command Panel 🎅❄️", rows: rows}]
        
        const button = {
         buttonText: 'Click Me▷',
-        description: "👿 T-REX ALL COMMAND 👿",
+        description: "❄️🎅 T-REX ALL COMMAND ☃️",
         sections: sections,
         listType: 1
        }
@@ -991,12 +994,15 @@ if (hrs >= 17 && hrs <= 19) wish = '*ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 🌥*'
 if (hrs >= 19 && hrs <= 24) wish = '*ɢᴏᴏᴅ ɴɪɢʜᴛ 🌙*'
 
     const rows = [
-        {title: '👿 ALL MENU 👿', description: `▷▷▷▷▷▷\n\n\n
+        {title: '☃️ ALL MENU ❄️🎅', description: `▷▷▷▷▷▷\n\n\n
  ╭────────────────
  │❰🍁🔱  T Rex BOT  🔱🍁❱
  ╰────────────────
  ╭────────────────
  │ Hello ▷ Dear
+ │
+ │🎅❄️☃️ MERRY CHRISTMAS ❄️☃️
+ │
  │ I Wish `+ wish + `
  │ 
  │Time⌚` + time + `
@@ -1200,7 +1206,7 @@ if (hrs >= 19 && hrs <= 24) wish = '*ɢᴏᴏᴅ ɴɪɢʜᴛ 🌙*'
   
        👿       Hiruwa        👿
 ` , rowId:"rowid1"},
-        {title: '👿 ADVANCE MENU 👿', description: `▷▷▷▷▷▷\n\n\n
+        {title: '☃️ ADVANCE MENU 🎅❄️', description: `▷▷▷▷▷▷\n\n\n
 ╔═══════════════════════╗
 
            ❰🍁🔱  T Rex BOT  🔱🍁❱
@@ -1495,7 +1501,7 @@ Example : .covid Sri Lanka°
 
 *➢ Description:* °Shorten link.°
 `, rowId:"rowid3"},
-        {title: '👿 LOGO PACK 01 👿', description:`▷▷▷▷▷▷▷\n\n\n
+        {title: '☃️ LOGO PACK 01 ❄️🎅', description:`▷▷▷▷▷▷▷\n\n\n
 ╔══════════════════════
 ║❌ 🍁*REX LOGO PACK 1*🍁 ❌
 ╠══════════════════════
@@ -1586,7 +1592,7 @@ Example : .covid Sri Lanka°
 ║        ╚ ENJOY WITH US ╝
 ╚══════════════════════
 `, rowId:"rowid4"},
-        {title: '👿 FF PACK 👿', description: `▷▷▷▷▷▷\n\n\n
+        {title: '☃️ FF PACK ❄️🎅', description: `▷▷▷▷▷▷\n\n\n
 ╔═════════════════════╗
 ║      ❰🍁🔱  T Rex BOT  🔱🍁❱  
 ║         ║🍁╚FF PACK╝🍁║
@@ -1696,7 +1702,7 @@ Example : .covid Sri Lanka°
 ║ ❰🍁🔱  T Rex BOT  🔱🍁❱  
 ╚═════════════════════╝
 `, rowId:"rowid5"},
-        {title: '👿 LOGO PACK 02 👿', description:`▷▷▷▷▷▷\n\n\n
+        {title: '☃️ LOGO PACK 02 🎅❄️', description:`▷▷▷▷▷▷\n\n\n
 ╔══════════════════════
 ║❌ 🍁*REX LOGO PACK 1*🍁 ❌
 ╠══════════════════════
@@ -1791,7 +1797,7 @@ Example : .covid Sri Lanka°
 ║        ╚ ENJOY WITH US ╝
 ╚══════════════════════
 `, rowId:"rowid6"},
-        {title: '👿 ATTP PACK 👿', description: `▷▷▷▷▷▷\n\n\n
+        {title: '☃️ ATTP PACK 🎅❄️', description: `▷▷▷▷▷▷\n\n\n
 ╔═════════════════════╗
 ║      ❰🍁🔱  T Rex BOT  🔱🍁❱  
 ║        ║🍁╚ATTP PACK╝🍁║
@@ -1883,7 +1889,7 @@ Example : .covid Sri Lanka°
 ╚═════════════════════╝
 
 `, rowId:"rowid20"},
-        {title: '👿 DOWNLOADER 👿', description:`▷▷▷▷▷▷\n\n\n
+        {title: '☃️ DOWNLOADER 🎅❄️', description:`▷▷▷▷▷▷\n\n\n
  ╭────────────────
  │  🍁👿 Downloader 👿🍁
  │
@@ -1901,7 +1907,7 @@ Example : .covid Sri Lanka°
  │
  ╰────────────────
 `, rowId:"rowid8"},
-        {title: '👿 ABOUT T-REX BOT 👿', description: `▷▷▷▷▷▷\n\n\n
+        {title: '☃️ ABOUT T-REX BOT 🎅❄️', description: `▷▷▷▷▷▷\n\n\n
 🍁 Developer : Hiruwa
 🍁 Bot             : T-REX V 3.0.0 
 🍁 Language : Sinhala & English
@@ -1909,18 +1915,18 @@ Example : .covid Sri Lanka°
 🍁 Develop language : Node js
 🍁 Developer no : wa.me/+94788306130
 `, rowId:"rowid9"},
-        {title: '👿 ADMIN PANEL 👿', description: `▷▷▷▷▷▷\n\n\n
+        {title: '☃️ ADMIN PANEL 🎅❄️', description: `▷▷▷▷▷▷\n\n\n
 ❰🍁🔱  T Rex BOT  🔱🍁❱\n\n🍁 COMMAND: *.ban*\nℹ️ DESCRIPTION: Remove someone from the group. Mention the message or tag a person to use command.\n\n🍁 COMMAND: *.add*\nℹ️ DESCRIPTION: Adds someone to the group.\n\n🍁 COMMAND: *.mute*\nℹ️ DESCRIPTION: Mute group chat. Messages can only be sent by admin. \n\n🍁 COMMAND: *.unmute*\nℹ️ DESCRIPTION: Remove group chat mute. Anyone can send a message.\n\n🍁 COMMAND: *.clear*\nℹ️ DESCRIPTION: Clearing chat.\n\n🍁 COMMAND: *.name*\nℹ️ DESCRIPTION: Change group name. \n\n🍁 COMMAND: *.promote*\nℹ️ DESCRIPTION: Promoting members to admin.\n\n🍁 COMMAND: *.demote*\nℹ️ DESCRIPTION: Demoting admins.\n\n🍁 COMMAND: *.myplugin*\nℹ️ DESCRIPTION: To view the current plugin list from ❰🍁🔱  T Rex BOT  🔱🍁❱.\n\n🍁 COMMAND: *.plug*\nℹ️ DESCRIPTION: Install external plugins.\n\n🍁 COMMAND: *.unplug*\nℹ️ DESCRIPTION: Removes the plugin.\n\n🍁 COMMAND: *.invite*\nℹ️ DESCRIPTION: Provides group invitation link.\n\n🍁 COMMAND: *.afk*\nℹ️ DESCRIPTION: It turns bot bot AFK - Away From Keyboard.\n\n🍁 COMMAND: *.restart*\n➢ DESCRIPTION: Restart the bot.\n\n🍁 COMMAND: *.shutdown*\n➢ DESCRIPTION: Shutdown the Bot.\n\n🍁 COMMAND: *.setvar*\n➢ DESCRIPTION: Change heroku config vars.\n\n🍁 COMMAND: *.getvar*\n➢ DESCRIPTION: Get heroku config var.\n\n🍁 COMMAND: *.speedtest*\n➢ DESCRIPTION:Check download and upload speed.\n\n🍁 COMMAND: *.filter*\nℹ️ DESCRIPTION: It adds filters. If someone writes your filter word, it sends the answer. If you write a .filter it will show your list of filters.\n\n🍁 COMMAND: *.stop*\nℹ️ DESCRIPTION: Stops the filters you added earlier.\n\n🍁 COMMAND: *.welcome*\nℹ️ DESCRIPTION: It sets the welcome message.\n\n🍁 COMMAND: *.goodbye*\nℹ️ DESCRIPTION: Sets the goodbye  message.\n\n🍁 COMMAND: *.kickme*\nℹ️ DESCRIPTION: It removes the bot from the group that provided the command.\n\n🍁 COMMAND: *.pp*\nℹ️ DESCRIPTION: The photo you mention mentions the bot profile photo.\n\n🍁 COMMAND: *.block*\nℹ️ DESCRIPTION: Block tagged user.\n\n🍁 COMMAND: *.unblock*\nℹ️ DESCRIPTION: Unblock tagged user.\n\n🍁 COMMAND: *.jid*\nℹ️ DESCRIPTION: Giving user JID.\n\n🍁 COMMAND *.tagall*\nℹ️ DESCRIPTION: Tag everyone in group.\n\n🍁 COMMAND: *.up*\nℹ️ DESCRIPTION: Check for updates.\n\n🍁 COMMAND: *.uptrex*\nℹ️ DESCRIPTION: To update your bot.\n\n🍁 COMMAND: *.dyno*\nℹ️ DESCRIPTION: Showing heroku dyno hours.\n\n🍁 COMMAND: *.sysd*\nℹ️ DESCRIPTION: Show system properties.\n\n🍁 COMMAND: *.scam*\nℹ️ DESCRIPTION: Scam actions for 5min.\n\n🍁 COMMAND: *.antilink*\nℹ️ DESCRIPTION: Anti link on Ex: .antilink on/off\n\n🍁 COMMAND: *.antibad*\nℹ️ DESCRIPTION: Remove bad word users Ex: .antibad on/off\n\n🍁 COMMAND: *.chatbot*\nℹ️DESCRIPTION: Auto chat on Ex: .chatbot on/off\n👿 Command: .voice on/off\n▷ Description: Activate and deactivate voice chat\n\n👿 Command: .public ▷ Description: set bot as public\n\n 👿 Command: .private\n▷ Description: Set your bot as private\n\n*⚠️ The above command only works for Admin, Bot and SUDO numbers.\n\n❰🍁🔱  T Rex BOT  🔱🍁❱*
 `, rowId:"rowid10"},
-        {title: '👿 Work Type PUBLIC 👿', description: "▷▷▷▷▷▷\n\n\n Type .public to public mode ▷", rowId:"rowid9"},
-        {title: '👿 Work Type PRIVATE 👿', description:"▷▷▷▷▷▷\n\n\n Type .private to private mode ▷", rowId:"rowid2"}
+        {title: '❄️ Work Type PUBLIC ❄️', description: "▷▷▷▷▷▷\n\n\n Type .public to public mode ▷", rowId:"rowid9"},
+        {title: '❄️ Work Type PRIVATE ❄️', description:"▷▷▷▷▷▷\n\n\n Type .private to private mode ▷", rowId:"rowid2"}
        ]
        
-       const sections = [{title: "👿 T-REX Command Panel 👿", rows: rows}]
+       const sections = [{title: "☃️❄️ T-REX Command Panel 🎅☃️", rows: rows}]
        
        const button = {
         buttonText: 'Click Me▷',
-        description: "👿 T-REX COMMAND PANEL 👿",
+        description: "🎅❄️ T-REX COMMAND PANEL ☃️❄️",
         sections: sections,
         listType: 1
        }
@@ -1955,12 +1961,15 @@ if (hrs >= 17 && hrs <= 19) wish = '*ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 🌥*'
 if (hrs >= 19 && hrs <= 24) wish = '*ɢᴏᴏᴅ ɴɪɢʜᴛ 🌙*'
 
     const rows = [
-        {title: '👿 ALL MENU 👿', description: `▷▷▷▷▷~\n\n\n
+        {title: '☃️ ALL MENU 🎅❄️', description: `▷▷▷▷▷~\n\n\n
  ╭────────────────
  │❰🍁🔱  T Rex BOT  🔱🍁❱
  ╰────────────────
  ╭────────────────
  │ Hello ▷ Dear
+ │
+ │🎅❄️☃️ MERRY CHRISTMAS ❄️☃️
+ │
  │ I Wish `+ wish + `
  │ 
  │Time⌚` + time + `
@@ -2164,7 +2173,7 @@ if (hrs >= 19 && hrs <= 24) wish = '*ɢᴏᴏᴅ ɴɪɢʜᴛ 🌙*'
   
        👿       Hiruwa        👿
 ` , rowId:"rowid1"},
-        {title: '👿 ADVANCE MENU 👿', description: `▷▷▷▷▷▷\n\n\n
+        {title: '☃️ ADVANCE MENU 🎅❄️', description: `▷▷▷▷▷▷\n\n\n
 ╔═══════════════════════╗
 
            ❰🍁🔱  T Rex BOT  🔱🍁❱
@@ -2459,7 +2468,7 @@ Example : .covid Sri Lanka°
 
 *➢ Description:* °Shorten link.°
 `, rowId:"rowid3"},
-        {title: '👿 LOGO PACK 01 👿', description:`▷▷▷▷▷▷\n\n\n
+        {title: '☃️ LOGO PACK 01 🎅❄️', description:`▷▷▷▷▷▷\n\n\n
 ╔══════════════════════
 ║❌ 🍁*REX LOGO PACK 1*🍁 ❌
 ╠══════════════════════
@@ -2550,7 +2559,7 @@ Example : .covid Sri Lanka°
 ║        ╚ ENJOY WITH US ╝
 ╚══════════════════════
 `, rowId:"rowid4"},
-        {title: '👿 FF PACK 👿', description: `▷▷▷▷▷▷\n\n\n
+        {title: '☃️ FF PACK 🎅❄️', description: `▷▷▷▷▷▷\n\n\n
 ╔═════════════════════╗
 ║      ❰🍁🔱  T Rex BOT  🔱🍁❱  
 ║         ║🍁╚FF PACK╝🍁║
@@ -2660,7 +2669,7 @@ Example : .covid Sri Lanka°
 ║ ❰🍁🔱  T Rex BOT  🔱🍁❱  
 ╚═════════════════════╝
 `, rowId:"rowid5"},
-        {title: '👿 LOGO PACK 02 👿', description:`▷▷▷▷▷▷\n\n\n
+        {title: '☃️ LOGO PACK 02 🎅❄️', description:`▷▷▷▷▷▷\n\n\n
 ╔══════════════════════
 ║❌ 🍁*REX LOGO PACK 1*🍁 ❌
 ╠══════════════════════
@@ -2755,7 +2764,7 @@ Example : .covid Sri Lanka°
 ║        ╚ ENJOY WITH US ╝
 ╚══════════════════════
 `, rowId:"rowid6"},
-        {title: '👿 ATTP PACK 👿', description: `▷▷▷▷▷▷\n\n\n
+        {title: '☃️ ATTP PACK 🎅❄️', description: `▷▷▷▷▷▷\n\n\n
 ╔═════════════════════╗
 ║      ❰🍁🔱  T Rex BOT  🔱🍁❱  
 ║        ║🍁╚ATTP PACK╝🍁║
@@ -2847,7 +2856,7 @@ Example : .covid Sri Lanka°
 ╚═════════════════════╝
 
 `, rowId:"rowid20"},
-{title: '👿 DOWNLOADER 👿', description:`▷▷▷▷▷▷\n\n\n
+{title: '☃️ DOWNLOADER 🎅❄️', description:`▷▷▷▷▷▷\n\n\n
  ╭────────────────
  │  🍁👿 Downloader 👿🍁
  │
@@ -2865,7 +2874,7 @@ Example : .covid Sri Lanka°
  │
  ╰────────────────
 `, rowId:"rowid8"},
-        {title: '👿 ABOUT T-REX BOT 👿', description: `▷▷▷▷▷▷\n\n\n
+        {title: '🎅 ABOUT T-REX BOT ☃️❄️', description: `▷▷▷▷▷▷\n\n\n
 🍁 Developer : Hiruwa
 🍁 Bot             : T-REX V 3.0.0 
 🍁 Language : Sinhala & English
@@ -2873,18 +2882,18 @@ Example : .covid Sri Lanka°
 🍁 Develop language : Node js
 🍁 Developer no : wa.me/+94788306130
 `, rowId:"rowid9"},
-        {title: '👿 ADMIN PANEL 👿', description: `▷▷▷▷▷▷\n\n\n
+        {title: '☃️ ADMIN PANEL 🎅1', description: `▷▷▷▷▷▷\n\n\n
 ❰🍁🔱  T Rex BOT  🔱🍁❱\n\n🍁 COMMAND: *.ban*\nℹ️ DESCRIPTION: Remove someone from the group. Mention the message or tag a person to use command.\n\n🍁 COMMAND: *.add*\nℹ️ DESCRIPTION: Adds someone to the group.\n\n🍁 COMMAND: *.mute*\nℹ️ DESCRIPTION: Mute group chat. Messages can only be sent by admin. \n\n🍁 COMMAND: *.unmute*\nℹ️ DESCRIPTION: Remove group chat mute. Anyone can send a message.\n\n🍁 COMMAND: *.clear*\nℹ️ DESCRIPTION: Clearing chat.\n\n🍁 COMMAND: *.name*\nℹ️ DESCRIPTION: Change group name. \n\n🍁 COMMAND: *.promote*\nℹ️ DESCRIPTION: Promoting members to admin.\n\n🍁 COMMAND: *.demote*\nℹ️ DESCRIPTION: Demoting admins.\n\n🍁 COMMAND: *.myplugin*\nℹ️ DESCRIPTION: To view the current plugin list from ❰🍁🔱  T Rex BOT  🔱🍁❱.\n\n🍁 COMMAND: *.plug*\nℹ️ DESCRIPTION: Install external plugins.\n\n🍁 COMMAND: *.unplug*\nℹ️ DESCRIPTION: Removes the plugin.\n\n🍁 COMMAND: *.invite*\nℹ️ DESCRIPTION: Provides group invitation link.\n\n🍁 COMMAND: *.afk*\nℹ️ DESCRIPTION: It turns bot bot AFK - Away From Keyboard.\n\n🍁 COMMAND: *.restart*\n➢ DESCRIPTION: Restart the bot.\n\n🍁 COMMAND: *.shutdown*\n➢ DESCRIPTION: Shutdown the Bot.\n\n🍁 COMMAND: *.setvar*\n➢ DESCRIPTION: Change heroku config vars.\n\n🍁 COMMAND: *.getvar*\n➢ DESCRIPTION: Get heroku config var.\n\n🍁 COMMAND: *.speedtest*\n➢ DESCRIPTION:Check download and upload speed.\n\n🍁 COMMAND: *.filter*\nℹ️ DESCRIPTION: It adds filters. If someone writes your filter word, it sends the answer. If you write a .filter it will show your list of filters.\n\n🍁 COMMAND: *.stop*\nℹ️ DESCRIPTION: Stops the filters you added earlier.\n\n🍁 COMMAND: *.welcome*\nℹ️ DESCRIPTION: It sets the welcome message.\n\n🍁 COMMAND: *.goodbye*\nℹ️ DESCRIPTION: Sets the goodbye  message.\n\n🍁 COMMAND: *.kickme*\nℹ️ DESCRIPTION: It removes the bot from the group that provided the command.\n\n🍁 COMMAND: *.pp*\nℹ️ DESCRIPTION: The photo you mention mentions the bot profile photo.\n\n🍁 COMMAND: *.block*\nℹ️ DESCRIPTION: Block tagged user.\n\n🍁 COMMAND: *.unblock*\nℹ️ DESCRIPTION: Unblock tagged user.\n\n🍁 COMMAND: *.jid*\nℹ️ DESCRIPTION: Giving user JID.\n\n🍁 COMMAND *.tagall*\nℹ️ DESCRIPTION: Tag everyone in group.\n\n🍁 COMMAND: *.up*\nℹ️ DESCRIPTION: Check for updates.\n\n🍁 COMMAND: *.uptrex*\nℹ️ DESCRIPTION: To update your bot.\n\n🍁 COMMAND: *.dyno*\nℹ️ DESCRIPTION: Showing heroku dyno hours.\n\n🍁 COMMAND: *.sysd*\nℹ️ DESCRIPTION: Show system properties.\n\n🍁 COMMAND: *.scam*\nℹ️ DESCRIPTION: Scam actions for 5min.\n\n🍁 COMMAND: *.antilink*\nℹ️ DESCRIPTION: Anti link on Ex: .antilink on/off\n\n🍁 COMMAND: *.antibad*\nℹ️ DESCRIPTION: Remove bad word users Ex: .antibad on/off\n\n🍁 COMMAND: *.chatbot*\nℹ️DESCRIPTION: Auto chat on Ex: .chatbot on/off\n👿 Command: .voice on/off\n▷ Description: Activate and deactivate voice chat\n\n👿 Command: .public ▷ Description: set bot as public\n\n 👿 Command: .private\n▷ Description: Set your bot as private\n\n*⚠️ The above command only works for Admin, Bot and SUDO numbers.\n\n❰🍁🔱  T Rex BOT  🔱🍁❱*
 `, rowId:"rowid10"},
-        {title: '👿 Work Type PUBLIC 👿', description: "▷▷▷▷▷▷\n\n\nType .public to public mode", rowId:"rowid9"},
-        {title: '👿 Work Type PRIVATE 👿', description:"▷▷▷▷▷▷\n\n\nType .private to private mode", rowId:"rowid2"}
+        {title: '❄️ Work Type PUBLIC ❄️', description: "▷▷▷▷▷▷\n\n\nType .public to public mode", rowId:"rowid9"},
+        {title: '❄️ Work Type PRIVATE ❄️', description:"▷▷▷▷▷▷\n\n\nType .private to private mode", rowId:"rowid2"}
        ]
        
-       const sections = [{title: "👿 T-REX Command Panel 👿", rows: rows}]
+       const sections = [{title: "❄️☃️ T-REX Command Panel 🎅❄️", rows: rows}]
        
        const button = {
         buttonText: 'Click Me▷',
-        description: "👿 T-REX COMMAND PANEL 👿",
+        description: "❄️☃️ T-REX COMMAND PANEL 🎅❄️",
         sections: sections,
         listType: 1
        }
