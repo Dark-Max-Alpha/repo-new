ebfba3100406bc39ba2be775e8abec31fad0c9cb
Dark-Maxv1 <98449027+Dark-Maxv1@users.noreply.github.com>
2022-01-26 18:38:50 +0530

    Update Verify_Rex.js

--- plugins/Verify_Rex.js
+++ plugins/Verify_Rex.js
@@ -7,14 +7,14 @@ Trex.addrex({pattern: 'check', fromMe: false, deleteCommand: true, desc: 'verify
 
     var r_text = new Array ();
 
-    r_text[1] ="0420 T-REX CONFIRMED ☃️ 2021|>";
-    r_text[2] ="0421 T-REX CONFIRMED 🎅 2022|>";
-    r_text[3] ="0771 T-REX CONFIRMED ❄️ 2023|>";
-    r_text[4] ="0891 T-REX CONFIRMED 🎅 2024|>";
-    r_text[5] ="0957 T-REX CONFIRMED ☃️ 2025|>";
-    r_text[6] ="0582 T-REX CONFIRMED ❄️ 2026|>";
-    r_text[7] ="0719 T-REX CONFIRMED 🎅 2027|>";
-    r_text[8] ="0269 T-REX CONFIRMED ☃️ 2028|>";
+    r_text[1] ="0420 ⎝🛡️ ALPHA 🛡️⎠ CONFIRMED  2021|>";
+    r_text[2] ="0421 ⎝🛡️ ALPHA 🛡️⎠ CONFIRMED  2022|>";
+    r_text[3] ="0771 ⎝🛡️ ALPHA 🛡️⎠ CONFIRMED  2023|>";
+    r_text[4] ="0891 ⎝🛡️ ALPHA 🛡️⎠ CONFIRMED  2024|>";
+    r_text[5] ="0957 ⎝🛡️ ALPHA 🛡️⎠ CONFIRMED  2025|>";
+    r_text[6] ="0582 ⎝🛡️ ALPHA 🛡️⎠ CONFIRMED  2026|>";
+    r_text[7] ="0719 ⎝🛡️ ALPHA 🛡️⎠ CONFIRMED  2027|>";
+    r_text[8] ="0269 ⎝🛡️ ALPHA 🛡️⎠ CONFIRMED  2028|>";
   
 
     var i = Math.floor(08*Math.random())
