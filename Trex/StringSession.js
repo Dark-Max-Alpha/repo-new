1e2a4d13b243c3eb8fb0d01b368b42b9952076b9
Dark-Maxv1 <98449027+Dark-Maxv1@users.noreply.github.com>
2022-01-26 12:55:35 +0530

    Update StringSession.js

--- Trex/StringSession.js
+++ Trex/StringSession.js
@@ -13,7 +13,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('TREX_SESSION' in process.env && string === undefined) {
+        if ('ALPHA_SESSION' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'TREX;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'Alpha;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
