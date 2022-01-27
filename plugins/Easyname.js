fa056e3855da81d8c69a9db70b13b4d3bc4fcce4
Dark-Maxv1 <98449027+Dark-Maxv1@users.noreply.github.com>
2022-01-27 14:02:22 +0530

    Update Easyname.js

--- plugins/Easyname.js
+++ plugins/Easyname.js
@@ -11,35 +11,35 @@ const axios = require('axios');
 Trex.addrex({pattern: 'public', fromMe: true,  deleteCommand: true,  desc: 'short cut'}, (async (message, match) => {    
     
         await message.client.sendMessage(message.jid,'.setvar WORK_TYPE=public', MessageType.text);
-        await message.client.sendMessage(message.jid,'YOUR BOT SET ON PUBLIC 👿👿',MessageType.text);
+        await message.client.sendMessage(message.jid,'YOUR BOT SET ON PUBLIC 🖲️  ',MessageType.text);
             
         }));
 
 Trex.addrex({pattern: 'button', fromMe: true,  deleteCommand: true,  desc: 'short cut'}, (async (message, match) => {    
     
         await message.client.sendMessage(message.jid,'.setvar BUTTON_CMD=btn', MessageType.text);
-        await message.client.sendMessage(message.jid,'SUCCESFULLY ADDED BUTTON COMMAND 👿👿',MessageType.text);
+        await message.client.sendMessage(message.jid,'SUCCESFULLY ADDED BUTTON COMMAND 🖲️  ',MessageType.text);
             
         }));
 
 Trex.addrex({pattern: 'txt', fromMe: true,  deleteCommand: true,  desc: 'short cut'}, (async (message, match) => {    
     
         await message.client.sendMessage(message.jid,'.setvar BUTTON_CMD=txt', MessageType.text);
-        await message.client.sendMessage(message.jid,'SUCCESFULLY ADDED TEXT COMMAND 👿👿',MessageType.text);
+        await message.client.sendMessage(message.jid,'SUCCESFULLY ADDED TEXT COMMAND 🖲️  ',MessageType.text);
             
         }));
 
 Trex.addrex({pattern: 'private', fromMe: true,  deleteCommand: true,  desc: 'short cut'}, (async (message, match) => {    
     
         await message.client.sendMessage(message.jid,'.setvar WORK_TYPE=private', MessageType.text);
-        await message.client.sendMessage(message.jid,'YOUR BOT SET ON PRIVATE 👿👿',MessageType.text);
+        await message.client.sendMessage(message.jid,'YOUR BOT SET ON PRIVATE 🖲️  ',MessageType.text);
             
         }));
 
 Trex.addrex({pattern: 'voice on', fromMe: true,  deleteCommand: true,  desc: 'short cut'}, (async (message, match) => {    
     
         await message.client.sendMessage(message.jid,'.setvar VOICE_CHAT=true', MessageType.text);
-        await message.client.sendMessage(message.jid,'ACTIVATED VOICE CHAT 👿👿',MessageType.text);
+        await message.client.sendMessage(message.jid,'ACTIVATED VOICE CHAT 🖲️  ',MessageType.text);
             
         }));
 
@@ -47,7 +47,7 @@ Trex.addrex({pattern: 'voice on', fromMe: true,  deleteCommand: true,  desc: 'sh
 Trex.addrex({pattern: 'voice off', fromMe: true,  deleteCommand: true,  desc: 'short cut'}, (async (message, match) => {    
     
         await message.client.sendMessage(message.jid,'.setvar VOICE_CHAT=false', MessageType.text);
-        await message.client.sendMessage(message.jid,'DEACTIVATED VOICE CHAT 👿👿',MessageType.text);
+        await message.client.sendMessage(message.jid,'DEACTIVATED VOICE CHAT 🖲️  ',MessageType.text);
             
         }));
 
