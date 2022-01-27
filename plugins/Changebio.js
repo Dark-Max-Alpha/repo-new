3f22b40383ef2c71f5169850f8ac593e2318e317
Dark-Maxv1 <98449027+Dark-Maxv1@users.noreply.github.com>
2022-01-27 14:00:57 +0530

    Update Changebio.js

--- plugins/Changebio.js
+++ plugins/Changebio.js
@@ -14,9 +14,9 @@ const kawa = fs.readFileSync('./Voice/kawa.mp3')
 Trex.addrex({pattern: 'bio ?(.*)', fromMe: true,  deleteCommand: false,  dontAddCommandList: true}, (async (message, match) => {
  
 
-    if (match[1] === '') return await message.client.sendMessage(message.jid,'Need Bio 👿');
+    if (match[1] === '') return await message.client.sendMessage(message.jid,'Need Bio 🖲️  ');
     
     await message.client.setStatus(match[1]);
-    await message.client.sendMessage(message.jid,'👿 Succesfully Change Your Bio '+(match[1])+' 👿🍁',MessageType.text);
+    await message.client.sendMessage(message.jid,'🖲️   Succesfully Change Your Bio '+(match[1])+' 🖲️  ',MessageType.text);
     }
 ));
