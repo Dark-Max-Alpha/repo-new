44b57375b556e531a39ee1f64f7fe872b188db19
Dark-Maxv1 <98449027+Dark-Maxv1@users.noreply.github.com>
2022-01-27 16:47:00 +0530

    Update status.js

--- plugins/status.js
+++ plugins/status.js
@@ -5,7 +5,7 @@ const ffmpeg = require('fluent-ffmpeg');
 const {execFile} = require('child_process');
 const cwebp = require('cwebp-bin');
 const Config = require('../config');
-const Down = '🪔✨ DOWNLOADING STATUS 🧨✨.'
+const Down = '🖲️  DOWNLOADING STATUS .'
 
     Trex.addrex({pattern: 'down$', fromMe: true }, (async (message, match) => {    
 
@@ -23,7 +23,7 @@ const Down = '🪔✨ DOWNLOADING STATUS 🧨✨.'
             ffmpeg(location)
                 .save('stat.png')
                 .on('end', async () => {
-                    await message.client.sendMessage(message.client.user.jid,fs.readFileSync('stat.png'), MessageType.image,{mimetype: Mimetype.png, caption: '❰🍁🔱  T Rex BOT  🔱🍁❱' ,quoted: message.data});
+                    await message.client.sendMessage(message.client.user.jid,fs.readFileSync('stat.png'), MessageType.image,{mimetype: Mimetype.png, caption: '⎝🛡️ ALPHA 🛡️⎠' ,quoted: message.data});
             });
         return 
         }
@@ -31,7 +31,7 @@ const Down = '🪔✨ DOWNLOADING STATUS 🧨✨.'
         ffmpeg(location)
             .save('statu.mp4')
             .on('end', async () => {
-                await message.client.sendMessage(message.client.user.jid,fs.readFileSync('statu.mp4'), MessageType.video,{mimetype: Mimetype.mp4, caption: '❰🍁🔱  T Rex BOT  🔱🍁❱' ,quoted: message.data});
+                await message.client.sendMessage(message.client.user.jid,fs.readFileSync('statu.mp4'), MessageType.video,{mimetype: Mimetype.mp4, caption: '⎝🛡️ ALPHA 🛡️⎠' ,quoted: message.data});
             });
         return 
     }));
