2b59f7d3060e28632c868eccb39975c3d67d7c0d
Dark-Maxv1 <98449027+Dark-Maxv1@users.noreply.github.com>
2022-01-27 16:56:42 +0530

    Update textcarbon.js

--- plugins/textcarbon.js
+++ plugins/textcarbon.js
@@ -3,7 +3,7 @@ const Trex = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 const Config = require('../config');
-const copy = " ❰🍁🔱  T Rex BOT  🔱🍁❱ "
+const copy = "⎝🛡️ ALPHA 🛡️⎠ "
 
 const Language = require('../language');
 const Lang = Language.getString('carbon');
