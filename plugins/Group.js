9e8e66f06118918104de79c3a7cca29b0f81b0b1
Dark-Maxv1 <98449027+Dark-Maxv1@users.noreply.github.com>
2022-02-07 21:36:02 +0530

    Update Group.js

--- plugins/Group.js
+++ plugins/Group.js
@@ -12,7 +12,7 @@ if (Config.WORKTYPE == 'private') {
             
             var image = await axios.get ('https://telegra.ph/file/cd6032c65c27e0510ddad.jpg', {responseType: 'arraybuffer'})
        
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP LIST ╗📛\n\n🖲️  GROUP  🖲️ \n\n 🔱 ' + Config.GROUPN1 + ' 🔱\n➢ ' + Config.GROUPL1 +'\n\n\n       ⎝🛡️ ALPHA 🛡️⎠'})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP LIST ╗📛\n\n🖲️  GROUP  🖲️ \n\n 🔱 ' + Config.GROUPN1 + ' 🔱\n\n\n       ⎝🛡️ ALPHA 🛡️⎠'})
 
     }));
   
@@ -23,7 +23,7 @@ else if (Config.WORKTYPE == 'public') {
 
           
             var image = await axios.get ('https://telegra.ph/file/cd6032c65c27e0510ddad.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP LIST ╗📛\n\n🖲️  GROUP  🖲️ \n\n 🔱 ' + Config.GROUPN1 + ' 🔱\n➢ ' + Config.GROUPL1 +'\n\n\n       ⎝🛡️ ALPHA 🛡️⎠'})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP LIST ╗📛\n\n🖲️  GROUP  🖲️ \n\n 🔱 ' + Config.GROUPN1 + ' 🔱\n\n\n       ⎝🛡️ ALPHA 🛡️⎠'})
 
     }));
   
@@ -31,7 +31,7 @@ else if (Config.WORKTYPE == 'public') {
 
           
             var image = await axios.get ('https://telegra.ph/file/cd6032c65c27e0510ddad.jpg', {responseType: 'arraybuffer'})
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP LIST ╗📛\n\n🖲️  GROUP  🖲️ \n\n 🔱 ' + Config.GROUPN1 + ' 🔱\n➢ ' + Config.GROUPL1 +'\n\n\n       ⎝🛡️ ALPHA 🛡️⎠   ' ,quoted: message.data})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: '\n      📛╔ GROUP LIST ╗📛\n\n🖲️  GROUP  🖲️ \n\n 🔱 ' + Config.GROUPN1 + ' 🔱\n\n\n       ⎝🛡️ ALPHA 🛡️⎠   ' ,quoted: message.data})
 
     }));
  
