96de4f6e29806db292aa6160f38c673b31110e8f
Dark-Maxv1 <98449027+Dark-Maxv1@users.noreply.github.com>
2022-02-21 12:36:40 +0530

    update now

--- plugins/5listhiruwa.js
+++ plugins/5listhiruwa.js
@@ -41,9 +41,7 @@ const sections = [{title: `${topText}`, rows: rows}]
 
 const button = {
  buttonText: 'Click Me!',
- description: `${topText}
- 
- ⎝🛡️ ALPHA 🛡️⎠ WhatsApp User Bot`,
+ description: `${topText}`,
  sections: sections,
  listType: 1
 }
