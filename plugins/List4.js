b0c2fb917524f277457a0f1256bed08bba79be39
Dark-Maxv1 <98449027+Dark-Maxv1@users.noreply.github.com>
2022-02-21 07:13:16 +0530

    update now

--- plugins/List4.js
+++ plugins/List4.js
@@ -38,9 +38,7 @@ const sections = [{title: `${topText}`, rows: rows}]
 
 const button = {
  buttonText: 'Click Me!',
- description: `${topText}
- 
-⎝🛡️ ALPHA 🛡️⎠`,
+ description: `${topText}`,
  sections: sections,
  listType: 1
 }
