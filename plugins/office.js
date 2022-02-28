3a0659e457f22280ea5494add6b7b22b7414576d
Dark-Maxv1 <98449027+Dark-Maxv1@users.noreply.github.com>
2022-02-28 15:49:50 +0530

    Update office.js

--- plugins/office.js
+++ plugins/office.js
@@ -7,18 +7,18 @@ const axios = require('axios');
 
 if (Config.WORKTYPE == 'public') {
 
-     Trex.addrex({pattern: 'ws', fromMe: false,  deleteCommand: true }, (async (message, match) => {
-
+    Trex.addrex({pattern: 'office', fromMe: false,  deleteCommand: true }, (async (message, match) => {
 
             var image = await axios.get ('https://telegra.ph/file/011d37f1cef06440daf7a.jpg', {responseType: 'arraybuffer'})
+
         await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭───────────────
 │ ⎝🛡️ SOFTWARE STORE BY ACHI BRO LK 🛡️⎠
-│      Wondershare Softwares 
+│      Ms Office Softwares 
 │  
-│  ◎Filmora 7.8.9 =.f9
-│  ◎Filmora 10.1.20 =.f20
-│  ◎Filmora 10.5.10 =.f10
-│  ◎Filmora 10.7.8.12 =.f12
+│  
+│  
+│  
+│  
 │
 │         🛡️POWERD BY🛡️
 │        ⎝🛡️ ALPHA 🛡️⎠
@@ -26,4 +26,5 @@ if (Config.WORKTYPE == 'public') {
 
     }));
 
-} 
+
+}
