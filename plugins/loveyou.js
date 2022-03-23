52c75bb558cae01c7f31eb0c266d889f2cf9ab1e
Dark-Max-Alpha <100671455+Dark-Max-Alpha@users.noreply.github.com>
2022-03-23 20:41:20 +0530

    Create loveyou.js

---
+++ plugins/loveyou.js
@@ -0,0 +1,52 @@
+const alpha= require('../events');
+const fs = require('fs');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+
+const chalk = require('chalk');
+const axios = require('axios');
+
+
+  alpha.addrex({pattern: 'love', fromMe: false,desc:' සමූහයක අයෙකුට ආදරේ ප්‍රකාශ කරන්න 🤗' }, (async (message, match) => {
+
+    await message.sendMessage('I');
+
+    await message.sendMessage('I L');
+
+    await message.sendMessage('I Lo');
+
+    await message.sendMessage('I Lov');
+
+    await message.sendMessage('I Love');
+
+    await message.sendMessage('I Love Y');
+
+    await message.sendMessage('I Love Yo');
+
+    await message.sendMessage('😍 I Love Youuu 😍');
+
+    await new Promise(r => setTimeout(r, 1000));
+
+    await message.sendMessage('S');
+
+    await message.sendMessage('So');
+
+    await message.sendMessage('So M');
+
+    await message.sendMessage('So Mu');
+
+    await message.sendMessage('So Muc');
+
+    await message.sendMessage('💕 So Much 💞');
+
+    await new Promise(r => setTimeout(r, 1000));
+
+   var image = await axios.get ('https://telegra.ph/file/2c1130ed7ccb22a507547.jpg', {responseType: 'arraybuffer'})
+
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: ` Do you love me 🙃 `,quoted: message.data})
+}));
+
+alpha.addrex({pattern: 'infolove', fromMe: false}, (async (message, match) => {
+
+    await message.sendMessage("Codded by Achiya😎 KDA \n*ආදරේ කියන්න හොදම විදිහ ❤️⃟✦⃝̮̮̮̯̯̯😌🔥*  \n❄️Usage: .love\n Dont Copy Plugin I'm Achi Fernando ")
+
+}));
